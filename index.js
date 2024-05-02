932714d0d2f2565101906222f81a2c7652f0bf21
abhilasha singh <abhilashasingh1391@gmail.com>
2024-05-02 18:16:26 +0530

    fix code

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+
+
+function isPrime(num) {
+    
+    if (num < 2) return false;
+
+  //pooja inititalize i = 2/ intentionally I have initialize i=1;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        
+        if (num % i === 0) return false;
+    }
+
+    
+    return true;
+}
+
+// Test the function
+console.log(isPrime(1)); 
+console.log(isPrime(2));    
+console.log(isPrime(7));
